bf3cf18c462cc87e6eafa67646326fd8e77d87da
Anantha Krishnan <ananthu0601@gmail.com>
2022-08-08 18:50:00 +0530

    return true for equal numbers

---
+++ src/equalNum.js
@@ -0,0 +1,10 @@
+// return true if both numbers are equal
+
+const equalNum = (a,b) => {
+    if (a===b){
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(equalNum(2,2));
