0ed31e2636a23508a2ac1e69c63a126edec55c7a
Anantha Krishnan <ananthu0601@gmail.com>
2022-08-08 18:52:30 +0530

    return true if num equals seven

---
+++ src/equalToSeven.js
@@ -0,0 +1,10 @@
+// return true if the number is equal to seven
+
+const equalToSeven = (num) => {
+    if( num===7) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(equalToSeven(7));
