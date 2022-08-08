c1c15f06387ccd10ed1ddf6290a29671b0438cc7
Anantha Krishnan <ananthu0601@gmail.com>
2022-08-08 18:47:12 +0530

    return true if both are true

---
+++ src/doubleAnd.js
@@ -0,0 +1,10 @@
+// return true only if both a and b are true
+
+const doubleAnd = (a,b) => {
+    if (a==true && b==true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(doubleAnd(true,true));
