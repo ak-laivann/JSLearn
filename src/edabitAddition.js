2cf54e803dd75de54f5699ca5af8f0632a074d25
Anantha Krishnan <ananthu0601@gmail.com>
2022-08-08 18:19:16 +0530

    edabit addition to the string

---
+++ src/edabitAddition.js
@@ -0,0 +1,10 @@
+// add a string of edabit to the user given value and store and show them in result
+
+let name = "";
+
+const edabitAddition = (a) => {
+    let result = [];
+    result.push(a+"EDABIT");
+    return result;
+}
+console.log(edabitAddition('myself'));
